166af39f4b79f439fe1ce3ccbb7965dbf6ef03a1
Leon Chuang <leonrlr4@gmail.com>
2024-02-27 12:57:53 +0800

    init auth0 config

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const sequelize = require('./config/database.js');
-const User = require('./models/User.js');
+const { auth } = require('express-openid-connect');
+// const User = require('./models/User.js');
+
 require('dotenv').config();
 
 const app = express();
 const PORT = process.env.APP_PORT || 3000;
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.AUTH_CLIENT_SECRET,
+  baseURL: process.env.BASE_URL,
+  clientID: process.env.AUTH_ClIENT_ID,
+  issuerBaseURL: process.env.AUTH_ISSUER,
+};
 
+app.use(auth(config));
 app.use(express.json());
 
 sequelize.sync()
@@ -16,12 +27,23 @@ sequelize.sync()
     console.error("Error during database sync:", error);
   });
 
+app.get('/profile', (req, res) => {
+  if (!req.oidc.isAuthenticated()) {
+    res.redirect('/login');
+  } else {
+    res.send(JSON.stringify(req.oidc.user));
+  }
+});
+
+// app.get('/', (req, res) => {
+//   res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+// });
+
 app.get('/', (req, res) => {
-  console.log(process.env.APP_PORT);
   res.send('Hello, World!');
 });
 
-
+  
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
